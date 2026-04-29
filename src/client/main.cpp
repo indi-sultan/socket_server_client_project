f776e31c6116548aa73f3ef69cf03a7bbea147cb
Shivam <shivamsuryathakur123@gmail.com>
2026-04-29 23:10:16 +0530

    serveris listening to client and sending echo correctly

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,32 +1,11 @@
-// C++ program to illustrate the client application in the
-// socket programming
-#include <cstring>
-#include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include "client/client.hpp"
+#include <boost/asio.hpp>
 
-int main()
-{
-    // creating socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+int main() {
+    boost::asio::io_context io_context;
 
-    // specifying address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-    // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
-            sizeof(serverAddress));
-
-    // sending data
-    const char* message = "Hello, server!";
-    send(clientSocket, message, strlen(message), 0);
-
-    // closing socket
-    close(clientSocket);
+    Client client(io_context, "127.0.0.1", 8080);
+    client.start();
 
     return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,43 +1,11 @@
-// C++ program to show the example of server application in
-// socket programming
-#include <cstring>
-#include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include "server/server.hpp"
+#include <boost/asio.hpp>
 
-using namespace std;
+int main() {
+    boost::asio::io_context io_context;
 
-int main()
-{
-    // creating socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    // specifying the address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-    // binding socket.
-    bind(serverSocket, (struct sockaddr*)&serverAddress,
-         sizeof(serverAddress));
-
-    // listening to the assigned socket
-    listen(serverSocket, 5);
-
-    // accepting connection request
-    int clientSocket
-        = accept(serverSocket, nullptr, nullptr);
-
-    // recieving data
-    char buffer[1024] = { 0 };
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message from client: " << buffer
-              << endl;
-
-    // closing the socket.
-    close(serverSocket);
+    Server server(io_context, 8080);
+    server.start();
 
     return 0;
 }
