6ceca1467db0255572f67e50ecf481e0515ec394
Shivam <shivamsuryathakur123@gmail.com>
2026-05-06 21:42:54 +0530

    updated server class for multiclient setup

--- include/server/server.hpp
+++ include/server/server.hpp
@@ -2,15 +2,27 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#include <vector>
+#include <memory>
+#include <mutex>
 
-class Server {
+class Session;
+
+class Server 
+{
 public:
     Server(boost::asio::io_context& io_context, int port);
     void start();
     void stop();
 
+    void broadcast(const std::string& message);
+    void addSession(std::shared_ptr<Session> session);
+    void removeSession(std::shared_ptr<Session> session);
+
 private:
     void handleClient(boost::asio::ip::tcp::socket socket);
+    std::vector<std::shared_ptr<Session>> sessions_;
+    std::mutex sessions_mutex_;
 
 private:
     bool running_ = true;
